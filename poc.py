fb39f9a98a60a1bbbd1285185c76d1028e2fb549
Quang Anh Trinh <qttri@student.unimelb.edu.au>
2025-05-21 03:35:36 +1000

    some boilerplate for playing sound

--- poc.py
+++ poc.py
@@ -1,34 +1,47 @@
+import os
+import numpy as np
 import librosa
 import matplotlib.pyplot as plt
 import argparse
+import sounddevice as sd
+import time
 from spleeter.separator import Separator
 
+def play_beat(frequency=1000, duration=0.05, sample_rate=44100):
+    """Play a short beep-like sound."""
+    t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
+    # Generate a sine wave (tone) or white noise (for a snare-like sound)
+    wave = 0.5 * np.sin(2 * np.pi * frequency * t)  # sine wave for a click
+    sd.play(wave, samplerate=sample_rate)
+    sd.wait()
+
+for i in range(10):
+    play_beat()
+    time.sleep(0.5)
+
+exit(0)
+
 parser = argparse.ArgumentParser(prog='TaikoChaos')
-parser.add_argument('--input', help='input (.mp3) for generating chart')
+parser.add_argument('--track', help='.mp3 track file to generate chart')
 
 args = parser.parse_args()
-input_file = args.input
+track_file = args.track
+track_name = os.path.splitext(os.path.basename(track_file))[0]
+output_folder = os.path.join('output', track_name)
 
-print(input_file)
+if not os.path.isdir(output_folder):
+    separator = Separator('spleeter:4stems')  # vocals, drums, bass, other
+    separator.separate_to_file(track_file, "output")
 
-separator = Separator('spleeter:4stems')  # vocals, drums, bass, other
-separator.separate_to_file(input_file, "tmp")
+mode = 'vocals'
+input_wav = os.path.join(output_folder, mode + '.wav')
 
+y, sr = librosa.load(input_wav)
+onset_frames = librosa.onset.onset_detect(y=y, sr=sr)
+onset_times = librosa.frames_to_time(onset_frames, sr=sr)
 
+print(onset_times)
 
-y, sr = librosa.load(input_file)
-
-# Detect onset frames
-# onset_frames = librosa.onset.onset_detect(y=y, sr=sr)
-# onset_times = librosa.frames_to_time(onset_frames, sr=sr)
-
-# # Print event times
-# print("Onsets (s):", onset_times)
-# 3. Visualize Detected Events (Optional)
-# python
-# Copy
-# Edit
-# librosa.display.waveshow(y, sr=sr)
-# plt.vlines(onset_times, -1, 1, color='r', linestyle='--')
-# plt.title("Onset Detection on Bass Track")
-# plt.show()
+librosa.display.waveshow(y, sr=sr)
+plt.vlines(onset_times, -1, 1, color='r', linestyle='--')
+plt.show()
